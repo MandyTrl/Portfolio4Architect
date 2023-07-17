9670a37c2524d15adb6a05ed230495c086f1f7dc
MThorel <mthorel@corp.free.fr>
2023-07-17 11:41:02 +0200

    close modal + css modal icones

--- FrontEnd/scripts/edition.js
+++ FrontEnd/scripts/edition.js
@@ -5,8 +5,8 @@ const projects = await fetchData(getProjects()) //besoin du await pour avoir la 
 const pictures = projects.map((el) => el.imageUrl) //tblx des url (img)
 
 //récupération des éléments du DOM
-const modal = document.getElementById("modal-content")
-const modalBtn = document.getElementById("modal-btn")
+const modal = document.getElementById("modal")
+const closeBtn = document.getElementById("close-modal")
 const content = document.getElementById("content")
 
 // Fct pour générer la galerie d'images
@@ -22,30 +22,38 @@ function createPicturesGallery() {
 		const span = document.createElement("span")
 		span.className = "edit-text"
 		span.innerText = "éditer"
+		const containerIcones = document.createElement("div")
+		containerIcones.className = "container-icones"
 
 		const containerIconeM = document.createElement("div")
 		containerIconeM.className = "container-iconeM"
-
 		const containerIconeT = document.createElement("div")
 		containerIconeT.className = "container-iconeT"
 
 		const moveIcone = document.createElement("i")
 		moveIcone.className =
-			"fa-solid a-arrows-up-down-left-right fa-xs move-icone"
+			"fa-solid fa-arrows-up-down-left-right fa-xs move-icone"
 		moveIcone.style.color = "#ffff"
-
 		const trashIcone = document.createElement("i")
 		trashIcone.className = "fa-solid fa-trash-can fa-xs trash-icone"
+		trashIcone.style.color = "#ffff"
 
 		//association des éléments enfants aux éléments parents du DOM
 		content.appendChild(pictureContainer)
 		pictureContainer.appendChild(picture)
 		pictureContainer.appendChild(span)
-		pictureContainer.appendChild(containerIconeM)
-		pictureContainer.appendChild(containerIconeM)
+		pictureContainer.appendChild(containerIcones)
+		containerIcones.appendChild(containerIconeM)
+		containerIcones.appendChild(containerIconeT)
 		containerIconeM.appendChild(moveIcone)
 		containerIconeT.appendChild(trashIcone)
 	})
 }
 
 createPicturesGallery()
+
+// Ferme la modal au clic sur le bouton "close"
+closeBtn.addEventListener("click", () => {
+	console.log("first")
+	modal.style.display = "none"
+})
