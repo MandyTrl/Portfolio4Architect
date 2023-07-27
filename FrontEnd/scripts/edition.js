1338fcabe1aa94c7e482d5c2d728e517a1392d8f
MThorel <mthorel@corp.free.fr>
2023-07-27 17:08:50 +0200

    display modal update + delete project in DB : ok

--- FrontEnd/scripts/edition.js
+++ FrontEnd/scripts/edition.js
@@ -1,8 +1,36 @@
 import { getProjects } from "./fetcher.js"
+import { projectsApiUrl } from "./links.js"
 import { fetchData } from "./projects.js"
 
 const projects = await fetchData(getProjects()) //besoin du await pour avoir la réponse dans les fct async sinon ça renvoie la promesse
-const pictures = projects.map((el) => el.imageUrl) //tblx des url (img)
+const pictures = projects.map((el) => {
+	return {
+		id: el.id,
+		url: el.imageUrl,
+	}
+})
+const token = localStorage.getItem("Token")
+
+// Fct pour supprimer un projet
+async function deleteProject(id) {
+	try {
+		const response = await fetch(`${projectsApiUrl}/${id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`, //envoi du token à l'appel de la route pr (accès autorisé)
+			},
+		})
+
+		if (!response.ok) {
+			throw new Error("Error deleting project")
+		} else {
+			console.log("Project successfully deleted !")
+		}
+	} catch (error) {
+		console.error(error.message)
+	}
+}
 
 //récupération des éléments du DOM
 const modal = document.getElementById("modal")
@@ -18,7 +46,7 @@ function createPicturesGallery() {
 		pictureContainer.className = "picture-container"
 
 		const picture = document.createElement("img")
-		picture.setAttribute("src", img)
+		picture.setAttribute("src", img.url)
 		picture.setAttribute("alt", "picture-to-edit")
 		const span = document.createElement("span")
 		span.className = "edit-text"
@@ -30,13 +58,16 @@ function createPicturesGallery() {
 		containerIconeM.className = "container-iconeM"
 		const containerIconeT = document.createElement("div")
 		containerIconeT.className = "container-iconeT"
+		containerIconeT.addEventListener("click", () => {
+			deleteProject(img.id)
+		})
 
 		const moveIcone = document.createElement("i")
 		moveIcone.className =
 			"fa-solid fa-arrows-up-down-left-right fa-xs move-icone"
 		moveIcone.style.color = "#ffff"
 		const trashIcone = document.createElement("i")
-		trashIcone.className = "fa-solid fa-trash-can fa-xs trash-icone"
+		trashIcone.className = `fa-solid fa-trash-can fa-xs trash-icone-${img.id}`
 		trashIcone.style.color = "#ffff"
 
 		//association des éléments enfants aux éléments parents du DOM
@@ -53,7 +84,8 @@ function createPicturesGallery() {
 
 createPicturesGallery()
 
-// Fcts pour ouvrir/fermer la modal
+// Display de la modal
+//fcts pour ouvrir/fermer la modal
 function openModalHandler() {
 	modal.style.display = "block"
 }
@@ -61,13 +93,16 @@ function closeModalHandler() {
 	modal.style.display = "none"
 }
 
-// Ajout des écouteurs d'événements "click"
+//ajout des écouteurs d'événements "click"
 openModal.addEventListener("click", openModalHandler)
 closeBtn.addEventListener("click", closeModalHandler)
 
-// Ferme la modal quand le clic se fait en-dehors de celle-ci
+//ferme la modal qd le clic se fait en-dehors de celle-ci
 window.addEventListener("mousedown", (e) => {
-	if (e.target !== modal) {
+	const targetElement = e.target
+	const isInsideModal = modal.contains(targetElement) //check si le user click dans la modal
+
+	if (!isInsideModal && targetElement !== openModal) {
 		closeModalHandler()
 	}
 })
